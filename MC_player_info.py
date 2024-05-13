b245deeba84272d1036ed6de5288baa8c26327cd
Niko <94224292+NikohasAcat@users.noreply.github.com>
2024-05-13 22:15:30 +0200

    Add files via upload

--- MC_player_info.py
+++ MC_player_info.py
@@ -25,8 +25,8 @@ print(' ')
 username = input(f"{Fore.BLUE}U{Fore.BLUE}s{Fore.BLUE}e{Fore.BLUE}r n{Fore.CYAN}a{Fore.CYAN}m{Fore.WHITE}e:{Fore.WHITE} ")
 uuid = get_uuid(username)
 if uuid:
-    print(f"{Fore.WHITE}The UUID for {username} is: {uuid}")
+    print(f"{Fore.BLUE}T{Fore.BLUE}h{Fore.BLUE}e U{Fore.CYAN}U{Fore.CYAN}I{Fore.CYAN}D f{Fore.CYAN}o{Fore.CYAN}r{Fore.CYAN} {username} {Fore.CYAN}i{Fore.WHITE}s{Fore.WHITE}: {uuid}")
     skin_download_link = get_skin_download_link(uuid)
-    print(f"{Fore.WHITE}Skin download link: {skin_download_link}")
+    print(f"{Fore.BLUE}S{Fore.BLUE}k{Fore.BLUE}i{Fore.BLUE}n d{Fore.BLUE}o{Fore.BLUE}w{Fore.CYAN}n{Fore.CYAN}l{Fore.CYAN}o{Fore.CYAN}a{Fore.CYAN}d l{Fore.CYAN}i{Fore.CYAN}n{Fore.WHITE}k: {skin_download_link}")
 else:
     print(f"{Fore.WHITE}Failed to get UUID for {username}")
